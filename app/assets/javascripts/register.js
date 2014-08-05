21f9a1d568ea5a9f0bc60a14615fdef4746e1b2b
thekendalmiller <millerk@lightcastletech.com>
2014-08-04 22:48:15 -0400

    forgot to show shirt selector registration stuff

--- app/assets/javascripts/register.js
+++ app/assets/javascripts/register.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
   var attendingFields = $('.attending');
 
-  if ($('#user_attending').is(':checked')) {
+  if ($('#user_attending option:selected').val() == 'true') {
     attendingFields.show();
   }
 
   $('#user_attending').change(function() {
-    if ($(this).is(":checked")) {
+    if ($('#user_attending option:selected').val() == 'true')  {
       attendingFields.show();
     } else {
       attendingFields.hide();
