5e395e301906813001539039634a13c3d3b5ed63
Kendal Miller <thekendalmiller@gmail.com>
2015-08-25 21:48:22 -0400

    Fix omniauth exception when user clicks cancel

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,3 +2,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, App.google_client_id, App.google_client_secret
   provider :github, App.github_client_id, App.github_client_secret, scope: 'user:email'
 end
+
+# Gracefully handle failures instead of exploding
+OmniAuth.config.on_failure = Proc.new { |env|
+  OmniAuth::FailureEndpoint.new(env).redirect_to_failure
+}
