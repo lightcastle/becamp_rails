c4d63876f7b62280c04a2d85f71ff6e2639a032b
Lucas Cioffi <lucas.cioffi@gmail.com>
2017-07-22 15:07:33 -0400

    Turn on admin check for editing sponsors

--- app/controllers/sponsors_controller.rb
+++ app/controllers/sponsors_controller.rb
@@ -1,5 +1,5 @@
 class SponsorsController < ApplicationController
-  # before_filter :require_admin_user
+  before_filter :require_admin_user
   before_action :set_sponsor, only: [:show, :edit, :update, :destroy]
 
   # GET /sponsors
